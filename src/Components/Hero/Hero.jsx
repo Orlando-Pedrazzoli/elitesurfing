ac61f6e135c26cedca3fefc4da7997225b22ff3c
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2024-04-13 22:47:58 +0100

    responsive

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div className='hero'>
       <div className='hero-left'>
-        <h2>NEW ARRIVALS ONLY</h2>
+        <h2>SURFERS ONLY</h2>
         <div>
           <div className='hero-hand-icon'>
             <p>Trust Our</p>
