d3aa5b84438d352d9abe8492b5f70e75965acaef
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2024-04-26 11:32:40 +0100

    whats app

--- src/Components/WhatsAppButton/WhatsAppButton.jsx
+++ src/Components/WhatsAppButton/WhatsAppButton.jsx
@@ -5,7 +5,7 @@ import whats_img from '../Assets/icons8-whatsapp-94.png';
 const WhatsAppButton = () => {
   return (
     <div className='whatsapp-button'>
-      <a href='https://wa.me/351912164220'>
+      <a href='https://wa.me/351912164220/?text=Hi%2C%20I%20have%20a%20question%20about%20your%20products.'>
         <img src={whats_img} alt='WhatsApp' />
       </a>
     </div>
