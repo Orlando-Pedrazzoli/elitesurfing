e3884179c3bb053c2881d5e61635611b98119062
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2024-04-27 23:29:25 +0100

    hero button

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
         </div>
         <div className='hero-latest-btn'>
           <div onClick={handleExploreClick}>Explore</div>
-          <img src={arrow_icon} alt='' />
+          <img onClick={handleExploreClick} src={arrow_icon} alt='' />
         </div>
       </div>
       <div className='hero-right'>
