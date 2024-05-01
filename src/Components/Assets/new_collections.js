6ca652808a71a985fd0a4d8b7d6f7ba27281987e
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2024-05-01 11:08:30 +0100

    new collection updated

--- src/Components/Assets/new_collections.js
+++ src/Components/Assets/new_collections.js
@@ -1,65 +1,65 @@
-import p1_img from './product_12.png';
-import p2_img from './product_27.png';
-import p3_img from './product_14.png';
-import p4_img from './product_28.png';
-import p5_img from './product_15.png';
-import p6_img from './product_9.png';
-import p7_img from './product_7.png';
-import p8_img from './product_39.png';
+import p1_img from './product_37.png';
+import p2_img from './product_10.png';
+import p3_img from './product_40.png';
+import p4_img from './product_39.png';
+import p5_img from './product_38.png';
+import p6_img from './product_42.png';
+import p7_img from './product_41.png';
+import p8_img from './product_31.png';
 
 let new_collections = [
   {
-    id: 12,
-    name: 'Deck Fiji - White / Pink',
+    id: 37,
+    name: 'Deck Fiji - White / Black',
     image: p1_img,
     new_price: 25.0,
-    old_price: 42.5,
+    old_price: 42.0,
   },
   {
-    id: 27,
-    name: 'Deck Hawaii - Black/Red',
+    id: 10,
+    name: 'Deck J-Bay - White / Black',
     image: p2_img,
     new_price: 25.0,
     old_price: 42.5,
   },
   {
-    id: 14,
-    name: 'Deck Fiji - Black/Gray',
+    id: 40,
+    name: 'Deck Fiji - White / Pink',
     image: p3_img,
-    new_price: 25.0,
-    old_price: 42.5,
-  },
-  {
-    id: 28,
-    name: 'Deck Hawaii - Black/Blue',
-    image: p4_img,
-    new_price: 25.0,
-    old_price: 42.5,
-  },
-  {
-    id: 15,
-    name: 'Deck Fiji - Black/Green',
-    image: p5_img,
-    new_price: 25.0,
-    old_price: 42.5,
-  },
-  {
-    id: 9,
-    name: 'Deck J-Bay Blue',
-    image: p6_img,
-    new_price: 25.0,
-    old_price: 42.5,
-  },
-  {
-    id: 7,
-    name: 'Deck J-Bay Black/White',
-    image: p7_img,
     new_price: 25.0,
     old_price: 42.5,
   },
   {
     id: 39,
     name: 'Deck Fiji - White / Blue',
+    image: p4_img,
+    new_price: 25.0,
+    old_price: 42.5,
+  },
+  {
+    id: 38,
+    name: 'Deck Fiji - White / Orange',
+    image: p5_img,
+    new_price: 25.0,
+    old_price: 42.5,
+  },
+  {
+    id: 42,
+    name: 'Deck Fiji - White / Gray',
+    image: p6_img,
+    new_price: 25.0,
+    old_price: 42.5,
+  },
+  {
+    id: 41,
+    name: 'Deck Fiji White / Green',
+    image: p7_img,
+    new_price: 25.0,
+    old_price: 42.5,
+  },
+  {
+    id: 31,
+    name: 'Deck Hawaii - White / Black',
     image: p8_img,
     new_price: 25.0,
     old_price: 42.5,
