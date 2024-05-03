484bbdab201a860153abeef2b1f3957ff473bed5
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2024-05-03 11:46:22 +0100

    button hero updated

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
           <p>Your Next Ride!</p>
         </div>
         <div className='hero-latest-btn'>
-          <div onClick={handleExploreClick}>Explore</div>
+          <div onClick={handleExploreClick}>Whacth</div>
           <img onClick={handleExploreClick} src={arrow_icon} alt='' />
         </div>
       </div>
